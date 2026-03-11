6403c14ceddafa9e6e4a68cf6f17cf90d02285e8
Nicolas Di Trana <ditrananicolas@gmail.com>
2026-03-11 19:03:25 -0300

    initial commit

---
+++ src/main/java/org/emprenApp/EmprenApp.java
@@ -0,0 +1,11 @@
+package org.emprenApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprenApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EmprenApp.class, args);
+    }
+}
