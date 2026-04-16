8f5fb1b90e8037ec17bcc2fef436a57ed511c105
Renzo <renzo11demarco@gmail.com>
2026-04-16 16:44:44 -0300

    add EmprendimientoDTO

--- src/main/java/org/emprenApp/emprendimiento/domain/EmprendimientoRepository.java
+++ src/main/java/org/emprenApp/emprendimiento/domain/EmprendimientoRepository.java
@@ -1,4 +1,17 @@
 package org.emprenApp.emprendimiento.domain;
 
-public interface EmprendimientoRepository {
+import org.emprenApp.shared.application.enums.EstadoUserEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmprendimientoRepository extends JpaRepository<Emprendimiento, Long> {
+
+    Optional<Emprendimiento> findByIdAndEstado(Long id, EstadoUserEnum estado);
+
+    Page<Emprendimiento> findAllByEstado(EstadoUserEnum estado, Pageable pageable);
 }
